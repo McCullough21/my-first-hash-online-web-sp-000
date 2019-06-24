b0d0dfc99d344939cd3473d3e15b4bc6a9502fca
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-24 17:32:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,7 +18,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-
+shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
@@ -29,6 +29,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+                 
   # add 2 muskets to the shipping_manifest hash below
 
 
